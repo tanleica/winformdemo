94b5d2cd361ef8d518eaafb2c8e86c07bef74a64
tanleica <87714888+tanleica@users.noreply.github.com>
2022-08-26 18:25:13 +0700

    Init

---
+++ WinFormDemo/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace WinFormDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStartThread = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // buttonStartThread
+            // 
+            this.buttonStartThread.Location = new System.Drawing.Point(28, 22);
+            this.buttonStartThread.Name = "buttonStartThread";
+            this.buttonStartThread.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartThread.TabIndex = 2;
+            this.buttonStartThread.Text = "Start Thread";
+            this.buttonStartThread.UseVisualStyleBackColor = true;
+            this.buttonStartThread.Click += new System.EventHandler(this.buttonStartThread_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 159);
+            this.Controls.Add(this.buttonStartThread);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStartThread;
+    }
+}
+
---
+++ WinFormDemo/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormDemo
+{
+    public partial class Form1 : Form
+    {
+
+        private int ThreadCount;
+        public Form1()
+        {
+            InitializeComponent();
+            ThreadCount = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = ConfigurationManager.ConnectionStrings["ZKTECOEntities"].ConnectionString;
+        }
+
+        private void buttonStartThread_Click(object sender, EventArgs e)
+        {
+            StartThreat();
+        }
+
+        private void StartThreat()
+        {
+            new Thread(() =>
+            {
+                int i = DemoThreadStart();
+                if (i < 10)
+                {
+                    Thread.Sleep(1000);
+                    StartThreat();
+                }
+            }).Start();
+        }
+
+        private int DemoThreadStart()
+        {
+            ThreadCount++;
+            label2.Invoke(new Action(() => label2.Text = "Demo thread started in count of " + ThreadCount));
+            Thread.Sleep(1000);
+            return ThreadCount;
+        }
+
+    }
+}
